dc91e7f83277bcceecd70b842ea005c0a92b303c
belief997 <xieyr997@gmail.com>
2020-12-02 02:13:36 +0800

    udp 收发OK

--- Christmas_Tree/pages/udp/udp_1.js
+++ Christmas_Tree/pages/udp/udp_1.js
@@ -6,10 +6,14 @@ Page({
    */
   data: {
     text : "wait to act...",
-    port : null,
-    udp : null
+    Rxtext : "wait to Rec...",
+    fromIp : null,
+    udp : null,
+    UdpSendPort : null,
+    isFirst : 1
+    
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -21,7 +25,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
@@ -67,56 +71,72 @@ Page({
   },
 
   // udp 按键
-  btnClick : function(){
-    console.log("btn 1 is clicked")
-    wx.getSystemInfo({
-      success (res) {
-        console.log(res.model)
-        console.log(res.pixelRatio)
-        console.log(res.windowWidth)
-        console.log(res.windowHeight)
-        console.log(res.language)
-        console.log(res.version)
-        console.log(res.platform)
+  btnInit : function(){
+    console.log("btn 0 is clicked")
+    console.log("udp init")
+    if(this.data.isFirst)
+    {
+      this.setData({udp: wx.createUDPSocket()});
+      if(this.data.udp === null){
+        console.log('暂不支持')
+        return ;
       }
-    })
+      this.setData({UdpSendPort: this.data.udp.bind()});
+      console.log(this.data.udp)
+      console.log("The udpPort is %d", this.data.UdpSendPort)
 
-    var cnt = 0;
-    this.setData({text:"udp connect start"})
-
-
-
-    const udp = wx.createUDPSocket()
-    console.log("here")
-    if(udp === null){
-      console.log('暂不支持')
-      return ;
+      this.data.udp.onListening(this.data.udp.onMessage)
+      this.setData({isFirst: 0});
     }
-    udp.bind()
-    console.log( udp)
-    udp.send({
-      // address: '222.20.119.177', 
-      address: '192.168.1.100', 
-      // address: '255.255.255.255', 
-      port: 8848,
-      message: 'hello bling bling'
-    });
-    console.log( udp)
-    this.setData({text:"udp send"})
- 
-    udp.onMessage(function (res){ 
-      //字符串转换，很重要
+
+
+
+    var that = this;
+    this.data.udp.onMessage(function (res){ 
+        //字符串转换，很重要
         let unit8Arr = new Uint8Array(res.message);
         let encodedString = String.fromCharCode.apply(null, unit8Arr);
         let message = decodeURIComponent(escape((encodedString)));
-      console.log(message)
-      console.log(res.remoteInfo.address)
-        // that.setData({
-        //   receiveMessage: message,
-        //   fromIp: res.remoteInfo.address
-        // })
+      console.log(message);
+      console.log(res.remoteInfo.address);
+      that.setData({Rxtext: message,   fromIp: res.remoteInfo.address});
     })
- 
+
+  },
+
+  btnSend : function(){
+    console.log("btn 1 is clicked")
+    // wx.getSystemInfo({
+    //   success (res) {
+    //     console.log(res.model)
+    //     console.log(res.pixelRatio)
+    //     console.log(res.windowWidth)
+    //     console.log(res.windowHeight)
+    //     console.log(res.language)
+    //     console.log(res.version)
+    //     console.log(res.platform)
+    //   }
+    // })
+    
+    this.setData({text:"udp connect start"})
+
+    console.log("isFirst: %d", this.data.isFirst)
+    console.log("here")
+
+    this.data.udp.send({
+      address: '192.168.1.100', 
+      port: 8848,
+      message: 'hello bling bling'
+    });
+
+    this.setData({text:"udp send"})
+
+  },
+
+  btnClose : function(){
+    this.data.udp.close()
+    this.setData({isFirst: 1});
+
   }
 
 
